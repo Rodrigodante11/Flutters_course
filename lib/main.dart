0581adee65e4a46a837dd779da2da8fd2b4b837c
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-03-26 19:15:25 -0300

    buttons

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,23 @@ void main(){
 
   runApp(MaterialApp(
         title: "Frases do dia",
-        home: Container(color: Colors.white,
-          child: Column(
-            children: const <Widget>[
-              Text(
-                  "Esse texto nao sai pro lado gracas ao container",
-                style: TextStyle(
-                  fontSize: 35,
-                  color: Colors.blue,
-                  fontStyle: FontStyle.normal,
-                  fontWeight: FontWeight.normal,
-                  letterSpacing: 2,
-                  decoration: TextDecoration.lineThrough,
-                  decorationColor: Colors.greenAccent,
-                  decorationStyle: TextDecorationStyle.solid,
-                  
-                ),
-              )
-            ],
-          )
+        home: Container(
+            color: Colors.white,
+            child:Column(
+              children: <Widget>[
+                TextButton(
+                    onPressed:() {
+                      print("Botao pressionado");
+                    },
+                    child: const Text(
+                        "Clique aqui",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                        )
+                    ))
+              ],
+            )
         ),
 
       ));
