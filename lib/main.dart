bae2cbcac8c1ec814970833ef1217795a644a578
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-03-26 20:31:05 -0300

    alinhamentos

--- lib/main.dart
+++ lib/main.dart
@@ -7,26 +7,18 @@ void main(){
         // debugShowCheckedModeBanner: false,
         title: "Frases do dia",
         home: Container(
-            padding: const EdgeInsets.fromLTRB(30, 80, 80, 30), // espacamento internos
-            margin: EdgeInsets.all(0),  // espacamento Externo  apenas para containers
-            // margin: EdgeInsets.fromLTRB(20, 20, 20, 20), // espacamento Externo
-            // margin: EdgeInsets.only(top:50,left: 50), // espacamento Externo
+            margin: EdgeInsets.only(top: 40),  // espacamento Externo  apenas para containers
             decoration: BoxDecoration(
               border: Border.all(width: 3, color:Colors.white)
             ),
-            child:Column(
-              children: const <Widget>[
-                Text("t1"),
 
-                Padding(
-                    padding: EdgeInsets.all(30),
-                    child: Text("T2"),
-                ),
-                Text("t3"),
-                // Text(""
-                //     "apenas um texto aleatorio para exebir no app flutter ",
-                //     textAlign: TextAlign.justify,
-                // )
+            child:Row (
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center, //
+              children: const <Widget>[
+                Text("T11"),
+                Text("T2"),
+                Text("T3"),
               ],
             )
         ),
