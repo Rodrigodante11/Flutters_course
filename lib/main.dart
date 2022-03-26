55104730186e5322660aecf3b873eef569963d08
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-03-26 14:25:33 -0300

    Text decoration

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,27 @@ import  'package:flutter/material.dart';
 void main(){
 
   runApp(MaterialApp(
-      title: "Frases do dia",
-      
-      home:  Row(children:  const <Widget> [
-      Text("Row1 "),
-        Text(" Row2 ")
-    //home:  Column(children:  const <Widget> [
-    // Text("Column"),
-    // Text("Column2")
-      ],)
+        title: "Frases do dia",
+        home: Container(color: Colors.white,
+          child: Column(
+            children: const <Widget>[
+              Text(
+                  "Esse texto nao sai pro lado gracas ao container",
+                style: TextStyle(
+                  fontSize: 35,
+                  color: Colors.blue,
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.normal,
+                  letterSpacing: 2,
+                  decoration: TextDecoration.lineThrough,
+                  decorationColor: Colors.greenAccent,
+                  decorationStyle: TextDecorationStyle.solid,
+                  
+                ),
+              )
+            ],
+          )
+        ),
+
       ));
 }
