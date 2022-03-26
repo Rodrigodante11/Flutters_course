3d8af415dd6ac1cc030198a34931558639f0ab6d
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-03-26 19:40:03 -0300

    espacamentos

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,32 @@
+import 'package:flutter/cupertino.dart';
 import  'package:flutter/material.dart';
 
 void main(){
 
   runApp(MaterialApp(
+        // debugShowCheckedModeBanner: false,
         title: "Frases do dia",
         home: Container(
-            color: Colors.white,
+            padding: const EdgeInsets.fromLTRB(30, 80, 80, 30), // espacamento internos
+            margin: EdgeInsets.all(0),  // espacamento Externo  apenas para containers
+            // margin: EdgeInsets.fromLTRB(20, 20, 20, 20), // espacamento Externo
+            // margin: EdgeInsets.only(top:50,left: 50), // espacamento Externo
+            decoration: BoxDecoration(
+              border: Border.all(width: 3, color:Colors.white)
+            ),
             child:Column(
-              children: <Widget>[
-                TextButton(
-                    onPressed:() {
-                      print("Botao pressionado");
-                    },
-                    child: const Text(
-                        "Clique aqui",
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black,
-                        )
-                    ))
+              children: const <Widget>[
+                Text("t1"),
+
+                Padding(
+                    padding: EdgeInsets.all(30),
+                    child: Text("T2"),
+                ),
+                Text("t3"),
+                // Text(""
+                //     "apenas um texto aleatorio para exebir no app flutter ",
+                //     textAlign: TextAlign.justify,
+                // )
               ],
             )
         ),
