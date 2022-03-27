314854dd71a80210c3486518e6f66f54ce99a405
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-03-26 21:59:03 -0300

    Layout base com Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,32 @@ import  'package:flutter/material.dart';
 void main(){
 
   runApp(MaterialApp(
-        // debugShowCheckedModeBanner: false,
+        debugShowCheckedModeBanner: false,
         title: "Frases do dia",
-        home: Container(
-            margin: EdgeInsets.only(top: 40),  // espacamento Externo  apenas para containers
-            decoration: BoxDecoration(
-              border: Border.all(width: 3, color:Colors.white)
-            ),
-            child: Image.asset(
-              "images/mesa.jpg",
-                  // fit: BoxFit.cover,// Cobre toda area disponivel
-                  // fit: BoxFit.contain,// para exibir a imagem inteira independente do espaco
-                  // fit: BoxFit.fill, // coloca a imagem toda no espacaTodo
-                  // fit: BoxFit.fitHeight // garante que a altura seja preenchida perdendo partes laterais da imagens
-                  // fit: BoxFit.none // exibe a imagem no seu tamanho original podendo perder partes da imagem
-                  fit: BoxFit.scaleDown //parecido com contains mas garante que a imagem caiba na horizontal sem distorcao
-            ) ,
-        ),
+        home: Scaffold(
+          appBar: AppBar(
+              title: const Text("Cabecalho do Programa"),
+              backgroundColor: Colors.green,
+              centerTitle: true ,
 
+          ),
+
+          body: const Padding(
+            padding: EdgeInsets.all(30),
+            child: Text("Conteudo do Body")
+          ),
+          bottomNavigationBar: BottomAppBar(
+            color: Colors.lightGreen,
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Row(
+                children: const <Widget> [
+                  Text("Texto11"),
+                  Text("Texto2")
+                ],
+              )
+            )
+          ),
+        ),
       ));
 }
