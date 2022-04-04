d601963417909af92ea367de10abd722862d8f5b
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-04-04 10:19:04 -0300

    navegacao entre abas

---
+++ navegacao_entre_telas/lib/TelaPrincipal.dart
@@ -0,0 +1,46 @@
+
+import 'package:flutter/material.dart';
+import 'package:navegacao_entre_telas/TelaSecundaria.dart';
+
+class TelaPrincipal extends StatefulWidget {
+  const TelaPrincipal({Key? key}) : super(key: key);
+
+  @override
+  State<TelaPrincipal> createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tela Principal"),
+        backgroundColor: Colors.orange,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(35),
+        child: Column(
+          children: <Widget>[
+            ElevatedButton(
+                onPressed: (){
+                  Navigator.push(
+                      context,  // BuildContext context da build linha 13
+                      MaterialPageRoute(
+                          builder: (context) => TelaSecundaria()
+                      )
+                  );
+                },
+                child: Padding(
+                    padding: EdgeInsets.all(15),
+                    child: Text('Ir para segunda tela')),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                )
+
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
---
+++ navegacao_entre_telas/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:navegacao_entre_telas/TelaPrincipal.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: TelaPrincipal(),
+  ));
+}
+
+
