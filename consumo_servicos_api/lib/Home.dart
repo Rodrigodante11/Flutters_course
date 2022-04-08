c28f13e7c481bd5aa971234d96a7dc6ee7b145f6
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-04-08 11:10:49 -0300

    consumindo API Web e mostrando na interface

--- consumo_servicos_api/lib/Home.dart
+++ consumo_servicos_api/lib/Home.dart
@@ -12,9 +12,11 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
+  String _resultado = "Resultado";
+  TextEditingController _controllerCep =  TextEditingController();
 
   _recuperarCep() async { //async == assincrono == metodo _recuperarCep assincrono
-    String cep = "37548000";
+    String cep = _controllerCep.text;
     String url = "https://viacep.com.br/ws/$cep/json/";
     http.Response responde;
 
@@ -28,6 +30,9 @@ class _HomeState extends State<Home> {
     String ddd = retorno["ddd"];
 
     print("Resposta localidade: $localidade , ddd $ddd" );
+    setState(() {
+      _resultado = "Cidade: $localidade, DDD: $ddd";
+    });
   }
 
   @override
@@ -40,10 +45,23 @@ class _HomeState extends State<Home> {
           padding: EdgeInsets.all(40),
           child: Column(
             children: <Widget>[
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Digite o cep, ex: 05248000"
+                ),
+                style: TextStyle(
+                  fontSize: 20
+                ),
+                controller: _controllerCep, // pegar o que o suario digitar
+              ),
               ElevatedButton(
                   onPressed: _recuperarCep,
-                  child: Text("Clique Aqui"))
+                  child: Text("Clique Aqui")
+              ),
+              Text(_resultado),
             ],
+
           ),
         )
     );
